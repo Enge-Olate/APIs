1c8adfc97dab4aca2abe27e7c63e11393b03fe39
Enge-Olate <marcioalexisolate@live.com>
2025-06-18 10:47:58 -0300

    Função para iterar sobre às cidades da minha região.

--- index.js
+++ index.js
@@ -4,41 +4,66 @@ require('dotenv').config();
 
 // Importat as bibliotecas necessárias
 const axios = require('axios');
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 
-async function tracaArmazenaClima() {
-    try{
-        const cidade = 'Santa Rita do Sapucaí';
+async function tracaArmazenaClima(cidade) {
+    try {
+
         const apikey = process.env.API_KEY;
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apikey}&lang=pt_br&units=metric`;
         console.log(`Buscando clima para a cidade: ${cidade}`);
         const response = await axios.get(url);
         const clima = response.data;
-        const dadosRecebidos={
+        const dadosRecebidos = {
             cidade: clima.name,
             temperatura: clima.main.temp,
             umidade: clima.main.humidity,
             sensacao_termica: clima.main.feels_like,
             data: new Date().toISOString(),
-            id: clima.id  
+
         }
         console.log('Dados recebidos:', dadosRecebidos);
         const pool = new Pool({
-            user:process.env.DB_USER,
-            host:process.env.DB_HOST,
-            database:process.env.DB_DATABASE,
-            port:process.env.DB_PORT,
-            password:process.env.PASSWORD
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DB_DATABASE,
+            port: process.env.DB_PORT,
+            password: process.env.PASSWORD
         });
+        let inseri_dados = await pool.query(
+            'insert into clima values($1, $2, $3, $4, $5);',
+            [
+                dadosRecebidos.cidade,
+                dadosRecebidos.temperatura,
+                dadosRecebidos.umidade,
+                dadosRecebidos.sensacao_termica,
+                dadosRecebidos.data
+            ]
+        );
+        console.log("Dados inseridos com sucesso!", inseri_dados);
         pool.connect(async (err, client, done) => {
-            err? console.log('Não conectado!'):
-            console.log("Conectado!");
+            err ? console.log('Não conectado!') :
+                console.log("Conectado!");
             client.release();
             pool.end();
         })
-        
-    }catch (error) {
+        // let mostra_bd = await pool.query('select * from clima;');
+        // console.log(mostra_bd);
+
+    } catch (error) {
         console.error('Erro ao buscar clima:', error.message);
     }
 }
-tracaArmazenaClima();
+async function itera_cidades(params) {
+    const cidades = [
+        "Santa Rita do Sapucaí",
+        "Varginha",
+        "Itajubá",
+        "Pouso Alegre",
+        "Maria da Fé"
+    ];
+    for (const cidade of cidades) {
+        await tracaArmazenaClima(cidade);
+    }
+}
+itera_cidades();
